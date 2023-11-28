34d3bc224bbf11fc0e58a8d2ffb71f5329ed9b9c
SochnuyFrykt <106699493+SochnuyFrykt@users.noreply.github.com>
2023-11-29 02:55:44 +0400

    start work in ocr page

--- printed_publications/src/pages/Ocr.tsx
+++ printed_publications/src/pages/Ocr.tsx
@@ -1,60 +1,151 @@
 // @ts-ignore
-import { useState } from 'react'
+import { Skeleton } from 'antd'
+import React, { useRef, useState } from 'react'
 import { createWorker } from 'tesseract.js'
 import '../Styles/App.css'
-import { Card, Col, Row, Skeleton, Upload } from 'antd'
-import {
-    InboxOutlined
-} from '@ant-design/icons'
+import style from '../ui/ocr/ocr.module.scss'
 
 export default () => {
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [ocrText, setOcrText] = useState<string>('')
+	const [isLoading, setIsLoading] = useState<boolean>(false)
+	const [ocrText, setOcrText] = useState<string>('')
 
-    const doOCR = async (file: any) => {
-        setOcrText('')
-        setIsLoading(true)
+	const doOCR = async (file: any) => {
+		setOcrText('')
+		setIsLoading(true)
 
-        const worker = await createWorker('rus');
+		const worker = await createWorker('rus')
 
-        console.log(file)
-        const { data } = await worker.recognize(file);
-        setOcrText(data.text)
-        setIsLoading(false)
-    };
+		console.log(file)
+		const { data } = await worker.recognize(file)
+		setOcrText(data.text)
+		setIsLoading(false)
+	}
 
-    return (
-        <div className="container">
-            <Row gutter={[50, 50]}>
-                <Col span={12}>
-                    <Upload.Dragger
-                        height={500}
-                        style={{width: '100%'}}
-                        accept='image/png, image/jpeg'
-                        name='file'
-                        multiple={false}
-                        action={(file) => {
-                            doOCR(file)
-                            return ''
-                        }}
-                        customRequest={() => {
-                        }}
-                        fileList={[]}
-                    >
-                        <p className="ant-upload-drag-icon">
-                            <InboxOutlined/>
-                        </p>
-                        <p className="ant-upload-text">Нажмите или перетащите файл для загрузки</p>
-                    </Upload.Dragger>
-                </Col>
-                <Col span={12}>
-                    {isLoading ?
-                        <Skeleton loading/>
-                        :
-                        ocrText && <Card>{ocrText}</Card>
-                    }
-                </Col>
-            </Row>
-        </div>
-    )
+	const DragDropFile = () => {
+		const [drag, setDrag] = React.useState(false)
+		const fileInputRef = useRef()
+
+		const dragStartHandler = e => {
+			e.preventDefault()
+			setDrag(true)
+		}
+
+		const dragLeaveHandler = e => {
+			e.preventDefault()
+			setDrag(false)
+		}
+
+		const onDropHandler = e => {
+			e.preventDefault()
+			let files = [...e.dataTransfer.files]
+			doOCR(files[0])
+		}
+
+		return drag ? (
+			<div
+				onDragStart={e => dragStartHandler(e)}
+				onDragLeave={e => dragLeaveHandler(e)}
+				onDragOver={e => dragStartHandler(e)}
+				onDrop={e => onDropHandler(e)}
+				className={style.dropImg}
+			>
+				Распознание текста
+			</div>
+		) : (
+			<div
+				onDragStart={e => dragStartHandler(e)}
+				onDragLeave={e => dragLeaveHandler(e)}
+				onDragOver={e => dragStartHandler(e)}
+				className={style.inputImg}
+				onChange={dragStartHandler}
+			>
+				Перетащите сюда файлы или
+				<button onClick={() => fileInputRef.current}>загрузите</button>
+			</div>
+		)
+	}
+
+	return (
+		<>
+			{/* <div className={style.container}>
+				<h1 style={{ marginBottom: '52px' }}>Добавление печатных изданий</h1>
+				<div className={style.containerOcr}>
+					<div className={style.inputImg}>
+						<Upload.Dragger
+							height={661}
+							style={{ width: '724px', border: '1px dashed #550DB2;' }}
+							accept='image/png, image/jpeg'
+							name='file'
+							multiple={false}
+							action={file => {
+								doOCR(file)
+								return ''
+							}}
+							customRequest={() => {}}
+							fileList={[]}
+						>
+							<p className='ant-upload-text'>
+								Перетащите сюда файлы или загрузите
+							</p>
+						</Upload.Dragger>
+					</div>
+					<div>
+						{isLoading ? (
+							<Skeleton loading />
+						) : (
+							ocrText && <Card>{ocrText}</Card>
+						)}
+					</div>
+				</div>
+			</div> */}
+
+			<div className={style.container}>
+				<h1 style={{ marginBottom: '52px' }}>Добавление печатных изданий</h1>
+				<div className={style.containerOcr}>
+					{DragDropFile()}
+					<div>
+						{isLoading ? (
+							<Skeleton loading />
+						) : (
+							ocrText && <p style={{ width: '592px' }}>{ocrText}</p>
+						)}
+					</div>
+				</div>
+			</div>
+			{/* <div className='container'>
+				<h1>Добавление печатных изданий</h1>
+				<Row gutter={[50, 50]}>
+					<Col span={12}>
+						<Upload.Dragger
+							height={500}
+							style={{ width: '100%' }}
+							accept='image/png, image/jpeg'
+							name='file'
+							multiple={false}
+							action={file => {
+								doOCR(file)
+								return ''
+							}}
+							customRequest={() => {}}
+							fileList={[]}
+						>
+							<p className='ant-upload-drag-icon'>
+								<InboxOutlined />
+							</p>
+							<p className='ant-upload-text'>
+								Нажмите или перетащите файл для загрузки
+							</p>
+						</Upload.Dragger>
+					</Col>
+					<Col span={12}>
+						{isLoading ? (
+							<Skeleton loading />
+						) : (
+							ocrText && <Card>{ocrText}</Card>
+						)}
+					</Col>
+				</Row>
+			</div> */}
+		</>
+	)
 }
