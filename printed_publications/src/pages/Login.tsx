c87bfb9e8a74794794e5b5341a17777806321ac2
Altee12 <112865235+alt2211@users.noreply.github.com>
2023-12-17 18:04:43 +0400

    Исправление съезжающего элемента на входе

--- printed_publications/src/pages/Login.tsx
+++ printed_publications/src/pages/Login.tsx
@@ -11,43 +11,43 @@ const Authorization = () => {
 
   return (
     <>
-        <div className='frame'>
-            <div className='div'>
-                <div className='div2'>
-                    <div className='div3'>
-                        <div
-                            className={page === 'login' ? 'divwrapper' : 'divwrapper2'}
-                            onClick={() => setPage('login')}>
-                            <div className={page === 'login' ? 'textwrapper' : 'textwrapper2'}>
-                                Вход
-                            </div>
-                        </div>
-                        <div
-                            className={page === 'register' ? 'divwrapper' : 'divwrapper2'}
-                            onClick={() => setPage('register')}>
-                            <div
-                                className={page === 'register' ? 'textwrapper' : 'textwrapper2'}>
-                                Регистрация
-                            </div>
-                        </div>
-                    </div>
-                    <div className='div4'/>
+      <div className='frame'>
+        <div className='div'>
+          <div className='div2'>
+            <div className='div3'>
+              <div
+                className={page === 'login' ? 'divwrapper' : 'divwrapper2'}
+                onClick={() => setPage('login')}>
+                <div className={page === 'login' ? 'textwrapper' : 'textwrapper2'}>
+                  Вход
                 </div>
-                {page === 'login' && <LoginPage/>}
-                {page === 'register' && <RegPage/>}
-                <div className='policy'>
-                    <div className='policy3'>
-                        Нажимая&nbsp;«Продолжить», вы принимаете&nbsp;
-                        <div className='policy2' style={{ width: '245px' }}>пользовательское соглашение</div>
-                        &nbsp;и
-                    </div>
-                    <div className='policy3'>
-                        <div className='policy2'>политику конфиденциальности</div>
-                        .
-                    </div>
+              </div>
+              <div
+                className={page === 'register' ? 'divwrapper' : 'divwrapper2'}
+                onClick={() => setPage('register')}>
+                <div
+                  className={page === 'register' ? 'textwrapper' : 'textwrapper2'}>
+                  Регистрация
                 </div>
+              </div>
             </div>
+            <div className='div4' />
+          </div>
+          {page === 'login' && <LoginPage />}
+          {page === 'register' && <RegPage />}
+          <div className='policy'>
+            <div className='policy3'>
+              Нажимая&nbsp;«Продолжить», вы принимаете&nbsp;
+              <div className='policy2' style={{ width: '245px' }}>пользовательское соглашение</div>
+              &nbsp;и
+            </div>
+            <div className='policy3'>
+              <div className='policy2'>политику конфиденциальности</div>
+              .
+            </div>
+          </div>
         </div>
+      </div>
     </>
   );
 };
@@ -55,7 +55,7 @@ const Authorization = () => {
 
 //Страница входа
 const LoginPage = () => {
-  const {setUser} = useContext(MainContext)
+  const { setUser } = useContext(MainContext)
 
   const [passwordVisible, setPasswordVisible] = useState(false);
   const togglePasswordVisibility = () => {
@@ -67,60 +67,56 @@ const LoginPage = () => {
     const formData = new FormData(e.target);
     const email = formData.get('email');
     const password = formData.get('password');
-    // if (email === '1@g' && password === '1') {
-    //   navigate('/');
-    // } else {
-    //   alert('Неправильный логин или пароль');
-    // }
-      // Отправка запроса на сервер для входа
-      const handleLogin = async () => {
-        try {
-          //Переделать через env
-          const response = await fetch('http://localhost:5000/auth/login', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              email: email,
-              password: password,
-            }),
-          });
-          const userData = await response.json();
-          const loggedUser = {id: userData.userId, 
-            email: userData.email};
-            localStorage.setItem('token', userData.token);
-          setUser(loggedUser)
-        } catch (error) {
-          console.error('Ошибка:', error);
-        }
+    handleLogin(email, password);
+  };
+
+  const handleLogin = async (email, password) => {
+    try {
+      const response = await fetch('http://localhost:5000/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+      const userData = await response.json();
+      const loggedUser = {
+        id: userData.userId,
+        email: userData.email
       };
-    handleLogin();
+      localStorage.setItem('token', userData.token);
+      setUser(loggedUser)
+    } catch (error) {
+      console.error('Ошибка:', error);
+    }
   };
 
   return (
     <form onSubmit={onFinish}>
       <label className='text'>
-          Электронная почта
+        Электронная почта
         <input
           name='email'
           type='email'
           placeholder='Введите адрес почты'
-		  className='inputF'
+          className='inputF'
         />
       </label>
       <label className='text'>
-          Пароль
+        Пароль
         <input
           name='password'
           type={passwordVisible ? 'text' : 'password'}
           placeholder='Введите пароль'
-		  className='inputF'
+          className='inputF'
         />
-      </label> 
-      <img src={!passwordVisible ? "closedEye.svg" : "Eye.svg"} alt="Пароль скрыт" 
-            className={!passwordVisible ? 'closedEye' : 'Eye'} onClick={togglePasswordVisibility} />
-      <div style={{ color: '#A609CB', textAlign: 'left',marginTop: '4px'}}>Забыли пароль?</div>
+        <img src={!passwordVisible ? "closedEye.svg" : "Eye.svg"} alt="Пароль скрыт"
+          className={!passwordVisible ? 'closedEye' : 'Eye'} onClick={togglePasswordVisibility} />
+      </label>
+      <div style={{ color: '#A609CB', textAlign: 'left', marginTop: '4px' }}>Забыли пароль?</div>
       <button className='next'>Продолжить</button>
     </form>
   );
@@ -144,17 +140,17 @@ const RegPage = () => {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ 
+        body: JSON.stringify({
           email: email,
           password: password,
-         }),
+        }),
       })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        //Обработка ответа от сервера
-      })
-      .catch(error => console.error('Ошибка:', error));
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          //Обработка ответа от сервера
+        })
+        .catch(error => console.error('Ошибка:', error));
     };
     handleRegister();
   };
@@ -167,7 +163,7 @@ const RegPage = () => {
           name='email'
           type='email'
           placeholder='Введите адрес почты'
-		  className='inputF'
+          className='inputF'
         />
       </label>
       <label className='text'>
@@ -176,18 +172,18 @@ const RegPage = () => {
           name='password'
           type={passwordVisible ? 'text' : 'password'}
           placeholder='Введите пароль'
-		  className='inputF'
+          className='inputF'
         />
       </label>
-      <img src={!passwordVisible ? "closedEye.svg" : "Eye.svg"} alt="Пароль скрыт" 
-            className={!passwordVisible ? 'closedEye' : 'Eye'} onClick={togglePasswordVisibility} />
+      <img src={!passwordVisible ? "closedEye.svg" : "Eye.svg"} alt="Пароль скрыт"
+        className={!passwordVisible ? 'closedEye' : 'Eye'} onClick={togglePasswordVisibility} />
       <label className='text'>
         Подтвердите пароль
         <input
           name='repeatPassword'
           type={passwordVisible ? 'text' : 'password'}
           placeholder='Введите пароль'
-		  className='inputF'
+          className='inputF'
         />
       </label>
       <button className='next'>
